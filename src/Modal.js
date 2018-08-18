390ce0c3c85ff48bedfc32be73e2c9c323464f6a
Luis Pineda JR <lspnd00@gmail.com>
2018-08-18 13:48:20 -0400

    continue with modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Portal from './Portal'
 import styled from 'styled-components'
+import Icon from './Utilities/Icon'
 
 export default class Modal extends Component {
   render() {
@@ -10,7 +11,9 @@ export default class Modal extends Component {
         {on && (
             <ModalWrapper>
                 <ModalCard>
-                <CloseButton onClick={toggle}>CLOSE</CloseButton>
+                <CloseButton onClick={toggle}>
+                    <Icon name="close"/>
+                </CloseButton>
                 <div>{children}</div>
                 </ModalCard>
                 <Background onClick={toggle}/>
@@ -49,6 +52,9 @@ const CloseButton = styled.button`
     position: absolute;
     top: 0;
     right: 0;
+    border: none;
+    background: transparent;
+    padding: 10px;
 `;
 
 const Background = styled.div`
