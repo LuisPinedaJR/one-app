c9e0499e8bbb5381888a2a4bf1594321159d206d
Luis Pineda JR <lspnd00@gmail.com>
2018-08-20 22:39:02 -0400

    using context api

--- src/App.js
+++ src/App.js
@@ -3,16 +3,43 @@ import logo from './logo.svg';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
+import User from './User'
+import { UserContext }  from './UserContext'
+
+class UserProvider extends Component {
+  state = {
+    id: '123',
+    name: 'scott',
+    email: 'scott@leveluptuts.com'
+  }
+	render(){
+    return(
+		<UserContext.Provider  
+      value={{
+        user: this.state
+      }}
+    >
+			{this.props.children}
+    </UserContext.Provider>
+    );
+	}
+}
+
 
 class App extends Component {
 	render() {
 		return (
+      <UserProvider>
 			<div className="App">
 				<header className="App-header">
 					<img src={logo} className="App-logo" alt="logo" />
 					<h1 className="App-title">Welcome to React</h1>
 				</header>
-        
+
+        <User>
+
+        </User>
+
 				<Toggle>
 					{({on, toggle})=>(
 						<Fragment>
@@ -24,6 +51,7 @@ class App extends Component {
 					)}
 				</Toggle>
 			</div>
+      </UserProvider>
 		);
 	}
 }
---
+++ src/User.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import { UserContext } from './UserContext';
+
+export default class User extends Component {
+	render() {
+		return (
+			<UserContext.Consumer>
+				{(context) => (
+					<div>
+						<h1>User Info</h1>
+						<h3>name: {context.user.name}</h3>
+						<h3>email: {context.user.email}</h3>
+						<h3>Id: {context.user.id}</h3>
+					</div>
+				)}
+				
+			</UserContext.Consumer>
+			
+		);
+	}
+}
+
