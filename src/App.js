c54794d3920d0136615d6bfe1cf2a53103c743e1
Luis Pineda JR <lspnd00@gmail.com>
2018-08-22 00:33:25 -0400

    moved user provider to its own

--- src/App.js
+++ src/App.js
@@ -3,65 +3,36 @@ import logo from './logo.svg';
 import './App.css';
 import { Toggle } from 'Utilities';
 import { Modal } from 'Elements';
-import User from './User'
-import { UserContext }  from './UserContext'
-
-class UserProvider extends Component {
-  state = {
-    id: '123',
-    name: 'scott',
-    email: 'scott@leveluptuts.com'
-  }
-
-  logout = () => {
-    this.setState({
-      id: null,
-      name: '',
-      email: '',
-    })
-  }
-
- 	render(){
-    return(
-		<UserContext.Provider  
-      value={{
-        user: this.state,
-        logout: this.logout
-      }}
-    >
-			{this.props.children}
-    </UserContext.Provider>
-    );
-	}
-}
+import User from './User';
+import UserProvider from './UserProvider';
 
 
 class App extends Component {
 	render() {
 		return (
-      <UserProvider>
-			<div className="App">
-				<header className="App-header">
-					<img src={logo} className="App-logo" alt="logo" />
-					<h1 className="App-title">Welcome to React</h1>
-				</header>
+			<UserProvider>
+				<div className="App">
+					<header className="App-header">
+						<img src={logo} className="App-logo" alt="logo" />
+						<h1 className="App-title">Welcome to React</h1>
+					</header>
 
-        <User>
+					<User>
 
-        </User>
+					</User>
 
-				<Toggle>
-					{({on, toggle})=>(
-						<Fragment>
-							<button onClick={toggle}>LOGIN</button>
-							<Modal on={on} toggle={toggle}>
-								<h1>Still in a Modal!</h1>
-							</Modal>
-						</Fragment>
-					)}
-				</Toggle>
-			</div>
-      </UserProvider>
+					<Toggle>
+						{({on, toggle})=>(
+							<Fragment>
+								<button onClick={toggle}>LOGIN</button>
+								<Modal on={on} toggle={toggle}>
+									<h1>Still in a Modal!</h1>
+								</Modal>
+							</Fragment>
+						)}
+					</Toggle>
+				</div>
+			</UserProvider>
 		);
 	}
 }
