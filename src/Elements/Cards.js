bd8996c68fa6709c42f6269202fd7db16514641a
Luis Pineda JR <lspnd00@gmail.com>
2018-08-27 23:02:44 -0400

    interpolation in depth

--- src/Elements/Cards.js
+++ src/Elements/Cards.js
@@ -6,7 +6,7 @@ export const Card = styled.div`
     background: white;
     border-radius: 5px;
     padding: 15px;
-    max-width: 320px
+    max-width: 320px;
     margin: 0 auto;
     colors: ${black};
     ${elevation[4]};
